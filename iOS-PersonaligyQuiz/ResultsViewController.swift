8e545996a351ea1088fa989d83c5ca351f084834
version-1 <j.onodera26@gmail.com>
2020-05-08 05:36:16 -0700

    implement until part2

---
+++ iOS-PersonaligyQuiz/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  iOS-PersonaligyQuiz
+//
+//  Created by Administlator on 2020/05/08.
+//  Copyright © 2020 Administlator. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
